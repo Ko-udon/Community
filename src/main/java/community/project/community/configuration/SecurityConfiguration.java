c5e0da8c9c81fa6e211ab7befda2adea6a2dc3ce
Kodongwoo <ehddn2202@naver.com>
2022-12-27 01:56:13 +0900

    프로젝트 생성2

--- src/main/java/community/project/community/configuration/SecurityConfiguration.java
+++ src/main/java/community/project/community/configuration/SecurityConfiguration.java
@@ -1,8 +1,16 @@
 package community.project.community.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+    return http.build();
+  }
 
 }
